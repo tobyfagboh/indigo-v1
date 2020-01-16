7a5b98845bf4793e0158a1d08fe8fb1049a79e5b
OscarFarouk <popspharouk@gmail.com>
2020-01-16 14:06:06 +0100

    IN-22: Drop down button- Created a script for the drop down button

---
+++ src/test/java/com/w2a/testcase/dropDown22.java
@@ -0,0 +1,19 @@
+package com.w2a.testcase;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import com.w2a.base.TestBase;
+
+public class dropDown22 extends TestBase {
+	
+	@Test
+	public void InvalidLogin () throws InterruptedException {
+		driver.findElement(By.xpath(OR.getProperty("email22_XPATH"))).sendKeys(OR.getProperty("emailTxt07"));
+		driver.findElement(By.xpath(OR.getProperty("password22_XPATH"))).sendKeys(OR.getProperty("passwordTxt07"));
+		driver.findElement(By.xpath(OR.getProperty("loginBtn22_XPATH"))).click();	
+		driver.findElement(By.xpath(OR.getProperty("DropDownBtn_XPATH"))).click();
+		driver.findElement(By.xpath(OR.getProperty("Type1Hosp_XPATH"))).click();
+		Thread.sleep(3000);
+	}
+}
